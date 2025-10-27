1d66ac64dba6fbc1f6da965465d6c3ac2710b80d
MatriX0N <stas5432167890@gmail.com>
2025-10-27 18:50:31 +0200

    Lab 3: Blueprints structure and unit tests added

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Імпорт Blueprint'а
-from app.users import users_bp
+    # Імпорт і реєстрація блюпрінтів
+    from app.users import users_bp
+    from app.products import products_bp
 
-# Реєстрація Blueprint
-app.register_blueprint(users_bp)
+    app.register_blueprint(users_bp)
+    app.register_blueprint(products_bp)
 
-# Імпорт головних маршрутів
-from app import routes
+    # Імпорт і реєстрація головних маршрутів
+    from app.routes import register_routes
+    register_routes(app)
+
+    return app
